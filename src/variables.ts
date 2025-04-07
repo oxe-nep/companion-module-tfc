2c34e2a4e51e6329abce7aa66c2d46a59d068c10
Lucas <kontakt@lucas-kirsche.de>
2025-04-07 10:24:27 +0200

    add sections for source destination variable naming

--- src/variables.ts
+++ src/variables.ts
@@ -1,9 +1,22 @@
 import type { TfcRouteInstance } from './main.js'
 
 export function UpdateVariableDefinitions(self: TfcRouteInstance): void {
-	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
-	])
+	const variableArray = [...self.panel.sources, ...self.panel.targets]
+		.sort((a, b) => a?.index! - b?.index!)
+		.map((sourceTarget) => {
+			return {
+				variableId: `sectionIndex${sourceTarget?.index}`,
+				name: `Section Index ${sourceTarget?.index}`,
+				value: sourceTarget?.name,
+			}
+		})
+	const variableObject: { [key: string]: string } = {}
+	variableArray.forEach((sourceTarget) => {
+		if (sourceTarget !== undefined && sourceTarget.variableId !== undefined) {
+			variableObject[sourceTarget.variableId] = sourceTarget.value!
+		}
+	})
+
+	self.setVariableDefinitions(variableArray)
+	self.setVariableValues(variableObject)
 }
